9feb803c2b249cd5ddf7393e7cbf1971a143e74b
Samantha Bull <samantha.bull@homeretailgroup.com>
2018-12-19 09:19:58 +0000

    Renamed project and finished styling

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  practise
+//
+//  Created by Samantha Bull on 11/12/2018.
+//  Copyright © 2018 Samantha Bull. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -37,6 +37,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizeable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizeable, for: .normal)
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizeable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizeable, for: .normal)
+        
         startNewRound()
     }
     
@@ -93,7 +109,6 @@ class ViewController: UIViewController {
         currentValue = 50
         slider.value = Float(currentValue)
     }
-    
     
 }
 
